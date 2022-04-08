2a280d5a7ead3358e8dc624808ffac645a202141
Ilysha <hacharoky86@gmail.com>
2022-04-08 21:15:33 +0300

    fix: csrf update

--- src/core/settings.py
+++ src/core/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://ilysha.herokuapp.com'
+]
